9f4495101dbe9947a8429cbc80116924df507438
Natali993 <manat3017@gmail.com>
2025-08-29 00:56:00 +0300

    feat: partner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { SectionSecond } from './SectionSecond';
 import { SectionThree } from './SectionThree';
 import { SectionFourth } from './SectionFourth';
 import { SectionFifth } from './SectionFifth';
+import { SectionSixth } from './api/SectionSixth';
 
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
         <SectionThree/>
         <SectionFourth/>
         <SectionFifth/>
+        <SectionSixth/>
       </main>
       <Footer />
     </div>
