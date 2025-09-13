7dbde86761219ad82bb2bed3e6c99ebe7b20d642
AndryKK <andrii.korbutiak@aleannlab.com>
2025-09-13 12:44:17 +0300

    Revert "add legacy-fallbacks.css for old browsers"
    This reverts commit adb20047dcf2c65f5adf8e3a57460bd58cd75678.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,12 @@ import { Layout } from '../components/Layout';
 import { HeroSection } from '../components/HeroSection';
 import { SpecializedServicesSection } from '../components/SpecializedServicesSection';
 import { ServicesInfographicSection } from '../components/ServicesInfographicSection';
-import { LegacyFallback } from '../components/LegacyFallback';
-import { LegacyHeroSection } from '../components/LegacyHeroSection';
-import { LegacySpecializedServicesSection } from '../components/LegacySpecializedServicesSection';
 
 export default function Home() {
   return (
     <Layout className="">
-      <LegacyFallback
-        fallback={<LegacyHeroSection />}
-      >
-        <HeroSection />
-      </LegacyFallback>
-
-      <LegacyFallback
-        fallback={<LegacySpecializedServicesSection />}
-      >
-        <SpecializedServicesSection />
-      </LegacyFallback>
-
+      <HeroSection />
+      <SpecializedServicesSection />
       <ServicesInfographicSection />
     </Layout>
   );
