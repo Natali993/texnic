ca1ee8bf1255396c12e86c792a4cd9ad278eaa4c
AndryKK <andrii.korbutiak@aleannlab.com>
2025-09-13 20:31:15 +0300

    fix: layout padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,11 +7,10 @@ interface LayoutProps {
 }
 
 export function Layout({ children, className = '' }: LayoutProps) {
-  console.log('v.6.0.3');
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-700">
       <Header />
-      <main className={`flex-1 bg-white pt-20 lg:pt-24 ${className}`}>
+      <main className={`flex-1 bg-white pt-28 lg:pt-24 ${className}`}>
         {children}
       </main>
       <Footer />
